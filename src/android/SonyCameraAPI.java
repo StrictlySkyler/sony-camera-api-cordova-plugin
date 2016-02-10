9e542e568d9fc62a15a108597352a0c5e60d9d13
Skyler Brungardt <skyler.brungardt@gmail.com>
2016-02-09 20:43:11 -0800

    Removed try with resourcs, and added debugging.

--- src/android/SonyCameraAPI.java
+++ src/android/SonyCameraAPI.java
@@ -22,38 +22,40 @@ public class SonyCameraAPI extends CordovaPlugin {
 
     if (action.equals("post")) {
 
-      url = "http://10.0.0.1:10000/sony/camera";
-      connection = (HttpURLConnection) new URL(url)
-        .openConnection();
-      connection.setDoOutput(true);
-      connection.setRequestProperty("Content-Type", "application/json");
+      Log.d("MyOutput", "post");
+
+      //url = "http://10.0.0.1:10000/sony/camera";
+      //connection = (HttpURLConnection) new URL(url)
+        //.openConnection();
+      //connection.setDoOutput(true);
+      //connection.setRequestProperty("Content-Type", "application/json");
 
       //String json = "{\"method\":\"setSelfTimer\",\"params\":[10],\"id\":1,\"version\":\"1.0\"}";
       //String json = "{\"method\":\"actTakePicture\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
       //String json = "{\"method\":\"setShootMode\",\"params\":[\"still\"],\"id\":1,\"version\":\"1.0\"}";
       //String json = "{\"method\":\"startContShooting\",\"params\":[],\"id\":1,\"version\":\"1.0\"}";
 
-      try (OutputStream output = connection.getOutputStream()) {
-        output.write(json.getBytes());
-      }
+      //OutputStream output = connection.getOutputStream();
+      //output.write(json.getBytes());
 
     } else {
 
-      url = "http://10.0.0.1:64321/scalarwebapi_dd.xml";
-      connection = (HttpURLConnection) new URL(url)
-        .openConnection();
+      Log.d("MyOutput", "get");
+
+      //url = "http://10.0.0.1:64321/scalarwebapi_dd.xml";
+      //connection = (HttpURLConnection) new URL(url)
+        //.openConnection();
     }
 
-      try (
-        BufferedReader reader = new BufferedReader(
-          new InputStreamReader(connection.getInputStream())
-        )
-      ) {
-        for (String line; (line = reader.readLine()) != null;) {
-          body = body + line;
-        }
-      }
+      //BufferedReader reader = new BufferedReader(
+        //new InputStreamReader(connection.getInputStream())
+      //);
 
-      System.out.println(body);
+      //for (String line; (line = reader.readLine()) != null;) {
+        //body = body + line;
+      //}
+
+      //System.out.println(body);
+    Log.d("MyOutput", "finally");
   }
 }
